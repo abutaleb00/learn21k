079df58258ab882d21d6ff75af541144753e06e2
taleb <taleb@commlinkinfotech.com>
2021-12-07 01:58:08 +0600

    initial design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  sum = (...num) => {
+    return num.reduce((a, b) => a + b);
+  };
+  newName = () => {
+    console.log("Hello");
+  };
+  render() {
+    let xam = this.sum(10, 30, 40, 50, 98);
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+        {xam}
+      </div>
+    );
+  }
 }
 
 export default App;
